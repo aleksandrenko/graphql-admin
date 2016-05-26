0bd0fb1e4f76945d47448b934313dc708cad20ad
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2016-05-26 18:56:35 +0300

    Schema generated form

--- app/index.js
+++ app/index.js
@@ -1,12 +1,32 @@
+import './styles.less'
+
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import Codemirror from 'react-codemirror';
 import schema from './schema';
 
-import './styles.less'
+
+import Form from "react-jsonschema-form";
+
+const formSchema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Title", default: "A new task"},
+    done: {type: "boolean", title: "Done?", default: false}
+  }
+};
+
+const formData = {
+  title: "First task",
+  done: true
+};
+
+const log = (type) => console.log.bind(console, type);
 
 // Required for codemirror styling and mode
+import Codemirror from 'react-codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/dracula.css';
 require('codemirror/mode/javascript/javascript');
@@ -19,5 +39,11 @@ ReactDOM.render(
       lineNumbers: true,
       theme: 'dracula',
     }} />
+    <h2>Schema generated form:</h2>
+    <Form schema={formSchema}
+          formData={formData}
+          onChange={log("changed")}
+          onSubmit={log("submitted")}
+          onError={log("errors")} />
   </section>,
   document.getElementById('main'));
